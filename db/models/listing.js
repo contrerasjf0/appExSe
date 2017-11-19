278e9ed0ae909727ed1fbbf2ae41d463e88cfb2c
Juan Francisco Esperanza Contreras <contrerasjf.0@gmail.com>
2017-11-18 21:06:55 -0600

    Start project

---
+++ db/models/listing.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Listing = sequelize.define('Listing', {
+    title: DataTypes.STRING,
+    description: DataTypes.TEXT
+  }, {
+    classMethods: {
+      associate: function(models) {
+        
+        Listing.belongsTo(models.User);
+      }
+    }
+  });
+  return Listing;
+};
